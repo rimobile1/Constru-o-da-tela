731ec118c64cbe54a88f16d97d11b6e96dc6ffa8
rimobile1 <icaro.mo1234@gmail.com>
2019-08-21 22:08:15 -0300

    Codificacao

--- app/src/main/java/br/com/etechoracio/parimpar/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/parimpar/MainActivity.java
@@ -2,12 +2,32 @@ package br.com.etechoracio.parimpar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+        EditText txtNumero;
+
+        TextView lblResultado2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                  txtNumero = findViewById(R.id.txtNumero);
+                  lblResultado2 = findViewById(R.id.lblResultado2);
+    }
+
+    public  void  onProcessar(View v){
+
+        int numero = Integer.parseInt(txtNumero.getText().toString());
+            if (numero%2 == 0){
+                lblResultado2.setText("Par");
+            }
+            else {
+                lblResultado2.setText("Impar");
+            }
     }
 }
